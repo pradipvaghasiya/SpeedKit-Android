a328be5896496ed032626ee5e4d99f2174a88147
Pradip Vaghasiya <pradip.vaghasiya@gmail.com>
2015-05-26 16:57:02 +0530

    Overlay actionable implemented

--- app/src/main/java/com/speedui/android/uiautomationdemo/cellfragments/TabsFragment.java
+++ app/src/main/java/com/speedui/android/uiautomationdemo/cellfragments/TabsFragment.java
@@ -3,6 +3,10 @@ package com.speedui.android.uiautomationdemo.cellfragments;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatCallback;
 import android.view.View;
 
 import com.example.android.common.view.SlidingTabLayout;
@@ -14,10 +18,12 @@ import java.util.List;
 /**
  * Created by pradipvaghasiya on 26/05/15.
  */
-public class TabsFragment extends SPSlidingTabsFragment{
+public class TabsFragment extends SPSlidingTabsFragment implements ViewPager.OnPageChangeListener{
+
+    private List<String> tabTitles = Arrays.asList("Title Cell","Check List Cell");
 
     public TabsFragment(){
-
+        this.isActionBarOverLay = true;
     }
 
     @Override
@@ -36,11 +42,13 @@ public class TabsFragment extends SPSlidingTabsFragment{
                 return Color.GRAY;
             }
         });
+
+        slidingTabLayout.setOnPageChangeListener(this);
     }
 
     @Override
     public List<String> getTabTitles() {
-        return Arrays.asList("Title Cell","Check List Cell");
+        return tabTitles;
     }
 
     @Override
@@ -53,5 +61,20 @@ public class TabsFragment extends SPSlidingTabsFragment{
             default:
                 return null;
         }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+       // ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
