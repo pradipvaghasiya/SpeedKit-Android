0ca0df996b88d99414a709c8254cb016640a71ba
Pradip Vaghasiya <pvaghasi@in.ibm.com>
2015-12-13 15:56:22 +0000

    Toolbar only fragment and activity added

--- uiautomation/src/main/java/com/speedui/android/uiautomation/toolbar/SPDarkToolBarOnlyFragment.java
+++ uiautomation/src/main/java/com/speedui/android/uiautomation/toolbar/SPDarkToolBarOnlyFragment.java
@@ -11,7 +11,11 @@ import com.speedui.android.uiautomation.R;
  * Project: UIAutomation-Android
  * Created by Pradip on 6/2/2015.
  */
-abstract public class SPDarkToolBarOnlyFragment  extends SPToolBarOnlyFragment{
+public class SPDarkToolBarOnlyFragment  extends SPToolBarOnlyFragment{
+    public SPDarkToolBarOnlyFragment(){
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_dark_toolbaronly,container,false);
