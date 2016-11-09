954ba0fb12903da62c7d448ce95037a8f2e3b55b
Pradip Vaghasiya <pvaghasi@gmail.com>
2016-11-09 15:58:20 +0530

    Memory leak fixed
    Change-Id: If3053cecd896f9b9c2e52aa4542d5a43fffbf59c

--- swiftui/src/main/java/com/happyfall/android/swiftui/listing/adapter/ListingAdapterListener.java
+++ swiftui/src/main/java/com/happyfall/android/swiftui/listing/adapter/ListingAdapterListener.java
@@ -5,42 +5,44 @@ import android.databinding.ObservableList;
 
 import com.happyfall.android.swiftui.listing.ListingViewModel;
 
+import java.lang.ref.WeakReference;
+
 /**
  * Created by pradip on 12/13/15.
  */
 public class ListingAdapterListener extends ObservableList.OnListChangedCallback<ObservableArrayList<ListingViewModel>> {
 
-    private ListingAdapter mAdapter;
+    private WeakReference<ListingAdapter> mAdapterWeakReference;
     public ListingAdapterListener(ListingAdapter adapter){
-        mAdapter = adapter;
+        mAdapterWeakReference = new WeakReference<ListingAdapter>(adapter);
     }
 
     @Override
     public void onChanged(ObservableArrayList<ListingViewModel> sender) {
-        mAdapter.notifyDataSetChanged();
+        mAdapterWeakReference.get().notifyDataSetChanged();
     }
 
     @Override
     public void onItemRangeChanged(ObservableArrayList<ListingViewModel> sender, int positionStart, int itemCount) {
-        mAdapter.notifyItemRangeChanged(positionStart, itemCount);
+        mAdapterWeakReference.get().notifyItemRangeChanged(positionStart, itemCount);
     }
 
     @Override
     public void onItemRangeInserted(ObservableArrayList<ListingViewModel> sender, int positionStart, int itemCount) {
-        mAdapter.notifyItemRangeInserted(positionStart, itemCount);
+        mAdapterWeakReference.get().notifyItemRangeInserted(positionStart, itemCount);
     }
 
     @Override
     public void onItemRangeMoved(ObservableArrayList<ListingViewModel> sender, int fromPosition, int toPosition, int itemCount) {
         if (itemCount == 1){
-            mAdapter.notifyItemMoved(fromPosition, toPosition);
+            mAdapterWeakReference.get().notifyItemMoved(fromPosition, toPosition);
         }else{
-            mAdapter.notifyDataSetChanged();
+            mAdapterWeakReference.get().notifyDataSetChanged();
         }
     }
 
     @Override
     public void onItemRangeRemoved(ObservableArrayList<ListingViewModel> sender, int positionStart, int itemCount) {
-        mAdapter.notifyItemRangeRemoved(positionStart, itemCount);
+        mAdapterWeakReference.get().notifyItemRangeRemoved(positionStart, itemCount);
     }
 }
